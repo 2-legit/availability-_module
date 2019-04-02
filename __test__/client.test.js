daa408142374cdcf85544822759c63f637f21ea9
Bradley Francis <fradleybrancis@gmail.com>
2019-04-02 14:46:59 -0700

    creates initial test suite with jest and enzyme, adds additional webpack config for test files, adds eslint ignore config for test files & config files

---
+++ __test__/client.test.js
@@ -0,0 +1,9 @@
+import Enzyme from 'enzyme';
+import React from 'react';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({adapter: new Adapter() });
+
+test('should have access to DOM', () => {
+  expect(typeof document).toBe('object');
+});
