032f692f28aba755b4dcc006c2c17836ca6f2108
Bradley Francis <fradleybrancis@gmail.com>
2019-04-05 17:02:20 -0700

    webpack configure works for css modules

--- webpack.config.js
+++ webpack.config.js
@@ -19,62 +19,20 @@ module.exports = {
         },
       },
       {
-        test:/\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['*', '.js', '.jsx'],
-  },
-};
-
-/* 
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-
-const path = require('path');
-
-module.exports = {
-  entry: './src/index.js',
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-react', '@babel/preset-env'],
-          },
-        },
         test: /\.css$/,
-        use: {
-          loader: ExtractTextPlugin.extract(
-            'style-loader',
-            combineLoaders([
-              {
-                loader: 'css-loader',
-                query: {
-                  modules: true,
-                  localIdentName: '[name]__[local]___[hash:base64:5]'
-                },
-              },
-            ])
-          ) 
-        },
+        loader: "style-loader"
       },
+      {
+        test: /\.css$/,
+        loader: "css-loader",
+        options: {
+          modules: true,
+          localIdentName: "[name]__[local]___[hash:base64:5]"
+        }
+      }
     ],
   },
   resolve: {
     extensions: ['*', '.js', '.jsx'],
   },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new ExtractTextPlugin('style.css')
-  ]
 };
-
-*/
