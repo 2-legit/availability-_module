453dadb0f4f7f8fd6f6b3953bf7336de7d5825b9
Bradley Francis <fradleybrancis@gmail.com>
2019-04-06 22:26:26 -0700

    implement switch statement instead of multiple else if statement

--- src/Components/Rating.jsx
+++ src/Components/Rating.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
 
 const Rating = (props) => {
-  if (props.rating === 3) {
-    return <span className="star">★★★☆☆</span>
-  } else if (props.rating === 4) {
-    return <span className="star">★★★★☆</span>
-  } else if (props.rating === 5) {
-    return <span className="star">★★★★★</span>
-  } else {
-    return <span className="star">☆☆☆☆☆</span>
+  switch (props.rating) {
+    case 1:
+      return <span className="star">★☆☆☆☆</span>
+    case 2:
+      return <span className="star">★★☆☆☆</span>
+    case 3:
+      return <span className="star">★★★☆☆</span>
+    case 4:
+      return <span className="star">★★★★☆</span>
+    case 5:
+      return <span className="star">★★★★★</span>
+    default:
+      return <span className="star">☆☆☆☆☆</span>
   }
 }
 
