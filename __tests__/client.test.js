5e719b98aaf7b951af6b8def0bb695b1ba839b34
Bradley Francis <fradleybrancis@gmail.com>
2019-04-08 16:03:11 -0700

    add test with sinon spy

--- __tests__/client.test.js
+++ __tests__/client.test.js
@@ -1,4 +1,5 @@
 import Enzyme from 'enzyme';
+import Sinon from 'sinon';
 import React from 'react';
 import App from '../src/Components/App';
 import Listing from '../src/Components/Listing';
@@ -19,7 +20,7 @@ test('should have access to DOM', () => {
   expect(typeof document).toBe('object');
 });
 
-describe('Component functionality', () => {
+describe('Check if all components exist', () => {
   it('expects App component to exist', () => {
     const wrapper = Enzyme.shallow(<App />);
     expect(wrapper.exists()).toBe(true)
@@ -38,12 +39,57 @@ describe('Component functionality', () => {
   })
 })
 
-// describe('<Rating />', () => {
-//   it('expects rating to be 5 stars', () => {
-//     const wrap = Enzyme.shallow(<Rating rating={3}/>);
-//     // expect(wrap.prop()).toEqual(5);
-//     wrap.setProps({ name: 'Moe' })
-//     expect(wrap.props('name')).toEqual('Moe')
-//   })
-// })
+describe('<Rating />', () => {
+  it('expects rating to be 0 stars when given a rating of 0', () => {
+    const wrap = Enzyme.shallow(<Rating rating={0}/>);
+    expect(wrap.containsMatchingElement(
+      <span className="star">☆☆☆☆☆</span>
+      )
+    ).toBeTruthy()
+  })
+  it('expects rating to be 1 stars when given a rating of 1', () => {
+    const wrap = Enzyme.shallow(<Rating rating={1}/>);
+    expect(wrap.containsMatchingElement(
+      <span className="star">★☆☆☆☆</span>
+      )
+    ).toBeTruthy()
+  })
+  it('expects rating to be 2 stars when given a rating of 2', () => {
+    const wrap = Enzyme.shallow(<Rating rating={2}/>);
+    expect(wrap.containsMatchingElement(
+      <span className="star">★★☆☆☆</span>
+      )
+    ).toBeTruthy()
+  })
+  it('expects rating to be 3 stars when given a rating of 3', () => {
+    const wrap = Enzyme.shallow(<Rating rating={3}/>);
+    expect(wrap.containsMatchingElement(
+      <span className="star">★★★☆☆</span>
+      )
+    ).toBeTruthy()
+  })
+  it('expects rating to be 4 stars when given a rating of 4', () => {
+    const wrap = Enzyme.shallow(<Rating rating={4}/>);
+    expect(wrap.containsMatchingElement(
+      <span className="star">★★★★☆</span>
+      )
+    ).toBeTruthy()
+  })
+  it('expects rating to be 5 stars when given a rating of 5', () => {
+    const wrap = Enzyme.shallow(<Rating rating={5}/>);
+    expect(wrap.containsMatchingElement(
+      <span className="star">★★★★★</span>
+      )
+    ).toBeTruthy()
+  })
+})
+
+describe('<AllListings />', () => {
+  it('something happens when button is clicked on', () => {
+    const onLeftClick = Sinon.spy()
+    const wrap = Enzyme.shallow(<AllListings />);
+    wrap.find("button").simulate('click', onLeftClick);
+    expect(onLeftClick.called).toEqual(true);
+  })
+})
 
